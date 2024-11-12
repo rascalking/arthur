8f7273a5791897f06c56f36bdf9b78e078cb7055
Dave Bonner <dbonner@gmail.com>
2024-11-11 21:53:46 -0500

    handle bsky's mid-text newlines

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import { creds } from './creds.js'
 
 
 const timeline = document.getElementById('timeline');
+const timelinePageSize = 30;
 
 async function getAgent(creds) {
   let agent = new BskyAgent({
@@ -13,24 +14,33 @@ async function getAgent(creds) {
 }
 
 async function showTimeline(agent) {
-  const { data } = await agent.getTimeline({limit: 30});
+  const { data } = await agent.getTimeline({limit: timelinePageSize});
   const { feed: posts } = data;
   console.log(posts);
   for (const post of posts) {
-    const wrapper = document.createElement("div");
+    const wrapper = document.createElement('div');
+    wrapper.classList.add('post');
 
-    const author = document.createElement("span");
-    const avatar = document.createElement("img");
-    avatar.setAttribute("src", post.post.author.avatar);
-    avatar.setAttribute("width", 50);
-    avatar.setAttribute("height", 50);
-    author.appendChild(avatar);
-    author.appendChild(document.createTextNode(post.post.author.displayName));
+    const author = document.createElement('span');
+    const avatar = document.createElement('img');
+    avatar.setAttribute('src', post.post.author.avatar);
+    avatar.setAttribute('width', 50);
+    avatar.setAttribute('height', 50);
+    author.append(avatar);
+    author.append(document.createTextNode(
+      `${post.post.author.displayName} (@${post.post.author.handle})`));
 
-    const text = document.createElement("p");
-    text.appendChild(document.createTextNode(post.post.record.text));
+    const text = document.createElement('div');
+    for (const piece of post.post.record.text.split('\n')) {
+      const div = document.createElement('p');
+      div.append(document.createTextNode(piece));
+      text.append(div);
+    }
 
-    wrapper.append(author, text);
+    const timestamp = document.createElement('div');
+    timestamp.append(document.createTextNode(post.post.record.createdAt));
+
+    wrapper.append(author, text, timestamp);
     timeline.append(wrapper);
   }
 }
