ef5d8e7dd3008c18e76aa92e226debf1ca977e70
Dave Bonner <dbonner@gmail.com>
2024-11-16 11:08:28 -0500

    turn the timeline into an actual web component
    i'm sure this will bite me in the ass when i try to make the agent
    object not a global, but we'll go with it for now.

--- src/app.js
+++ src/app.js
@@ -2,52 +2,94 @@ import { BskyAgent } from '@atproto/api'
 import { creds } from './creds.js'
 
 
-const timeline = document.getElementById('timeline');
-const timelinePageSize = 30;
+var agent; // TODO: something better than a global
+
 
 async function getAgent(creds) {
-  let agent = new BskyAgent({
+  agent = new BskyAgent({
     service: 'https://bsky.social'
   });
   await agent.login(creds);
   return agent;
 }
 
-async function showTimeline(agent) {
-  const { data } = await agent.getTimeline({limit: timelinePageSize});
-  const { feed: posts } = data;
-  console.log(posts);
-  for (const post of posts) {
-    const wrapper = document.createElement('div');
-    wrapper.classList.add('post');
+class BskyTimeline extends HTMLElement {
+  static observedAttributes = ['src', 'pageSize'];
+  static #defaultTimelinePageSize = 30;
 
-    const author = document.createElement('span');
-    const avatar = document.createElement('img');
-    avatar.setAttribute('src', post.post.author.avatar);
-    avatar.setAttribute('width', 50);
-    avatar.setAttribute('height', 50);
-    author.append(avatar);
-    author.append(document.createTextNode(
-      `${post.post.author.displayName} (@${post.post.author.handle})`));
-
-    const text = document.createElement('div');
-    for (const piece of post.post.record.text.split('\n')) {
-      const div = document.createElement('p');
-      div.append(document.createTextNode(piece));
-      text.append(div);
+  attributeChangedCallback(name, oldValue, newValue) {
+    console.log(`Attribute ${name} has changed from ${oldValue} to ${newValue}.`);
+    // TODO: handle changes to src
+    if (name === 'pageSize') {
+      if (typeof(newValue) === 'string') {
+        newValue = parseInt(newValue);
+      }
+      if (newValue !== NaN) {
+        this._internals.pageSize = newValue;
+      }
     }
-
-    const timestamp = document.createElement('div');
-    timestamp.append(document.createTextNode(post.post.record.createdAt));
-
-    wrapper.append(author, text, timestamp);
-    timeline.append(wrapper);
   }
+
+  connectedCallback() {
+    this._internals.src = this.getAttribute('src');
+    // TODO: actually handle src being defined (should be a feed url)
+    this._internals.pageSize = this.getAttribute('pageSize') || BskyTimeline.defaultTimelinePageSize;
+    this._internals.cursor = '';
+    this.getNextPage()
+  }
+
+  constructor() {
+    super();
+    this._internals = this.attachInternals();
+  }
+
+  async getNextPage() {
+    const { data } = await agent.getTimeline({
+      cursor: this._internals.cursor,
+      limit: this._internals.pageSize,
+    });
+    console.log(data);
+    const { feed: posts } = data;
+    this._internals.cursor = ''; // TODO
+    for (const post of posts) {
+      const postWrapper = document.createElement('div');
+      postWrapper.classList.add('post');
+
+      const avatar = document.createElement('img');
+      avatar.setAttribute('src', post.post.author.avatar);
+      avatar.setAttribute('width', 50);
+      avatar.setAttribute('height', 50);
+      avatar.classList.add('avatar');
+
+      const name = document.createElement('div');
+      name.append(document.createTextNode(
+        `${post.post.author.displayName} (@${post.post.author.handle})`));
+
+      const text = document.createElement('div');
+      for (const piece of post.post.record.text.split('\n')) {
+        const div = document.createElement('p');
+        div.append(document.createTextNode(piece));
+        text.append(div);
+      }
+
+      const timestamp = document.createElement('div');
+      timestamp.append(document.createTextNode(post.post.record.createdAt));
+
+      postWrapper.append(avatar, name, text, timestamp);
+      this.append(postWrapper);
+    }
+  }
+}
+customElements.define('bluesky-timeline', BskyTimeline);
+
+async function showBskyTimeline() {
+  const main = document.getElementsByTagName('main')[0];
+  main.append(document.createElement('bluesky-timeline'));
 }
 
 getAgent(creds)
   .then((agent) => {
-    showTimeline(agent);
+    showBskyTimeline();
   })
   .catch((error) => {
     console.error(error);
